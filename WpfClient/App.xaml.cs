db8a29ba0edb4364f6ce79f717bbb5870452d295
Atoen <97164063+Atoen@users.noreply.github.com>
2023-06-04 15:51:39 +0200

    Update App.xaml.cs

--- WpfClient/App.xaml.cs
+++ WpfClient/App.xaml.cs
@@ -7,7 +7,7 @@ namespace WpfClient;
 /// </summary>
 public partial class App : Application
 {
-    internal static string EndPointUri = "http://37.128.72.106:5107";
+    internal static string EndPointUri = "";
     // internal static string EndPointUri = "https://squadtalk.azurewebsites.net/";
 
 }
